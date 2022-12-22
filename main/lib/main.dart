f559c446085d9907bfb7b47b612de97ce687f401
Gowrav Thota <gtthota@gmail.com>
2022-12-21 23:47:28 -0500

    fix

--- main/lib/main.dart
+++ main/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:startup_namer/start.dart';
-import 'package:startup_namer/game_screen.dart';
 
 void main() {
-  runApp(StartMenuScreen());
+  runApp(MaterialApp(home: StartMenuScreen()));
 }
--- main/lib/start.dart
+++ main/lib/start.dart
@@ -2,35 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:startup_namer/game_screen.dart';
 
 class StartMenuScreen extends StatelessWidget {
+  const StartMenuScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Start Menu'),
+          title: const Text('Start Menu'),
         ),
         body: Column(
           children: [
             ElevatedButton(
-              child: Text('Start Game'),
+              child: const Text('Start Game'),
               onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => Game(questions: questions),
+                    builder: (context) => RunGame(),
                   ),
                 );
               },
             ),
             ElevatedButton(
-              child: Text('Options'),
+              child: const Text('Options'),
               onPressed: () {
+// ignore: todo
 // TODO: Add options screen
               },
             ),
             ElevatedButton(
               child: Text('Quit'),
               onPressed: () {
+// ignore: todo
 // TODO: Add quit functionality
               },
             ),
