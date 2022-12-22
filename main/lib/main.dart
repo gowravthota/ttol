fd5114470dfe92d992420a87963a4c517b12201c
whao37 <williamhao2016@gmail.com>
2022-12-21 22:51:04 -0500

    change
    change

--- main/lib/main.dart
+++ main/lib/main.dart
@@ -4,26 +4,3 @@ import 'package:startup_namer/start.dart';
 void main() {
   runApp(StartMenuScreen());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    var myVar = 'these nuts on your chin';
-    int yayDartSupportsVarTypes = 3;
-
-    return MaterialApp(
-      title: 'Ayo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Legion Mobile'),
-        ),
-        body: Center(
-          child: Text('Dart has beautiful string interpolation'
-              '\n$myVar\n$yayDartSupportsVarTypes'),
-        ),
-      ),
-    );
-  }
-}
