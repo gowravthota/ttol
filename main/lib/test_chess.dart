e85576db7f895cfb0f80bf410a84776b27ca2048
whao37 <williamhao2016@gmail.com>
2022-12-26 00:22:08 -0500

    chess display finished and fixed
    placeholder images and errything else

--- main/lib/test_chess.dart
+++ main/lib/test_chess.dart
@@ -10,14 +10,14 @@ class ChessGameState extends State<ChessGame> {
   List<List<String>> _board = [
     ["R", "N", "B", "Q", "K", "B", "N", "R"],
     ["P", "P", "P", "P", "P", "P", "P", "P"],
-    [" ", " ", " ", " ", " ", " ", " ", " "],
-    [" ", " ", " ", " ", " ", " ", " ", " "],
-    [" ", " ", " ", " ", " ", " ", " ", " "],
-    ["", " ", " ", " ", " ", " ", " ", " "],
+    ["y", "br", "y", "br", "y", "br", "y", "br"],
+    ["br", "y", "br", "y", "br", "y", "br", "y"],
+    ["y", "br", "y", "br", "y", "br", "y", "br"],
+    ["br", "y", "br", "y", "br", "y", "br", "y"],
     ["P", "P", "P", "P", "P", "P", "P", "P"],
     ["R", "N", "B", "Q", "K", "B", "N", "R"],
   ];
-
+// br is brown, y is yellow on an 8x8 board, everything else denotes a piece on the board N = Knight, R = Rook, B = Bishop, Q = Queen, K = King, P = Pawn
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,7 +36,7 @@ class ChessGameState extends State<ChessGame> {
                     );
                   } else if (cell == "R") {
                     return Image(
-                      image: AssetImage("assets/rook.png"),
+                      image: AssetImage("assets/rook.jpg"),
                       width: 50.0,
                       height: 50.0,
                     );
@@ -48,25 +48,25 @@ class ChessGameState extends State<ChessGame> {
                     );
                   } else if (cell == "B") {
                     return Image(
-                      image: AssetImage("assets/bishop.png"),
+                      image: AssetImage("assets/bishop.jpg"),
                       width: 50.0,
                       height: 50.0,
                     );
                   } else if (cell == "Q") {
                     return Image(
-                      image: AssetImage("assets/queen.png"),
+                      image: AssetImage("assets/queen.jpg"),
                       width: 50.0,
                       height: 50.0,
                     );
                   } else if (cell == "K") {
                     return Image(
-                      image: AssetImage("assets/king.png"),
+                      image: AssetImage("assets/king.jpg"),
                       width: 50.0,
                       height: 50.0,
                     );
                   }
                   return Container(
-                    color: cell == " " ? Colors.yellow : Colors.brown,
+                    color: cell == "y" ? Colors.yellow : Colors.brown,
                   );
                 }).toList();
               }).toList(),
